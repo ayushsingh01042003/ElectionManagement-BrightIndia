b4912ce3726fcd5feb4cd016a3903c00a1b8eb86
unknown <kruthibasavaraj12@gmail.com>
2023-08-03 21:57:03 +0530

    updated message sender

--- messageSenderAPI.js
+++ messageSenderAPI.js
@@ -1,6 +1,45 @@
-//Autofill - State, Assembly Constuency
 
-const users = [
-    {},
-    {}
-]
+const assemblyConstituencies = {
+    "Andhra Pradesh": [
+        "Assembly 1",
+        "Assembly 2",
+        "Assembly 3"
+    ],
+    "Karnataka": [
+        "Assembly A",
+        "Assembly B",
+        "Assembly C"
+    ],
+    "Tamil Nadu": [
+        "Assembly X",
+        "Assembly Y",
+        "Assembly Z"
+    ],
+    "Maharashtra": [
+        "Assembly M",
+        "Assembly N",
+        "Assembly O"
+    ],
+    "Uttar Pradesh": [
+        "Assembly U1",
+        "Assembly U2",
+        "Assembly U3"
+    ]
+};
+
+function populateAssemblyConstituency() {
+    const stateSelect = document.getElementById('state');
+    const assemblySelect = document.getElementById('assemblyConstituency');
+    
+    assemblySelect.innerHTML = '<option value="">Select Assembly Constituency</option>';
+    
+    const selectedState = stateSelect.value;
+    const constituencies = assemblyConstituencies[selectedState] || [];
+    
+    constituencies.forEach(constituency => {
+        const option = document.createElement('option');
+        option.value = constituency;
+        option.text = constituency;
+        assemblySelect.appendChild(option);
+    });
+}
